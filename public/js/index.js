af6825ac3ef90e8e359e195e86af5b8dc002da5a
bernatixer <bernat.torres.bellido@gmail.com>
2018-03-24 19:35:48 +0100

    working!

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,6 @@
 
-
+var socket = io.connect('http://localhost:3000');
+var sourceImageUrl = '';
 
 $(function(){
     var video = $('video')[0];
@@ -53,12 +54,76 @@ $(function(){
         canvas.getContext('2d').drawImage(video, 0, 0);
     };
   
+    socket.on('detect', function (data) {
+      console.log(sourceImageUrl);
+      sourceImageUrl = data.url;
+      processImage();
+    });
+
      // when the start button is clicked, take a snapshot
     $('#snapshot').on('click', function(){
         takeSnapshot();
+        socket.emit('image', { src: document.getElementById('canvas').toDataURL() });
     });
 
     window.onload = function() {
       getCameraAccess();
     };
+
+    function processImage() {
+      // **********************************************
+      // *** Update or verify the following values. ***
+      // **********************************************
+
+      // Replace the subscriptionKey string value with your valid subscription key.
+      var subscriptionKey = "4865490905414c64a184a4b431d7edc6";
+
+      // Replace or verify the region.
+      //
+      // You must use the same region in your REST API call as you used to obtain your subscription keys.
+      // For example, if you obtained your subscription keys from the westus region, replace
+      // "westcentralus" in the URI below with "westus".
+      //
+      // NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
+      // a free trial subscription key, you should not need to change this region.
+      var uriBase = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect";
+
+      // Request parameters.
+      var params = {
+          "returnFaceId": "true",
+          "returnFaceLandmarks": "false",
+          "returnFaceAttributes": "emotion",
+      };
+
+      document.querySelector("#sourceImage").src = sourceImageUrl;
+
+      // Perform the REST API call.
+      $.ajax({
+          url: uriBase + "?" + $.param(params),
+
+          // Request headers.
+          beforeSend: function(xhrObj){
+              xhrObj.setRequestHeader("Content-Type","application/json");
+              xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+          },
+
+          type: "POST",
+
+          // Request body.
+          data: '{"url": ' + '"' + sourceImageUrl + '"}',
+      })
+
+      .done(function(data) {
+          // Show formatted JSON on webpage.
+          $("#responseTextArea").val(JSON.stringify(data, null, 2));
+      })
+
+      .fail(function(jqXHR, textStatus, errorThrown) {
+          // Display error message.
+          var errorString = (errorThrown === "") ? "Error. " : errorThrown + " (" + jqXHR.status + "): ";
+          errorString += (jqXHR.responseText === "") ? "" : (jQuery.parseJSON(jqXHR.responseText).message) ?
+              jQuery.parseJSON(jqXHR.responseText).message : jQuery.parseJSON(jqXHR.responseText).error.message;
+          alert(errorString);
+      });
+   };
 });
--- server.js
+++ server.js
@@ -1,16 +1,31 @@
+const base64ToImage = require('base64-to-image')
 const express = require('express')
+const cloudinary = require('cloudinary')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
+
+cloudinary.config({ 
+  cloud_name: 'easycms', 
+  api_key: '835622277612669', 
+  api_secret: 'EPY2WeRXUwzaXpAEOK8VzuImiv0' 
+});
+
+var path = './';
+var optionalObj = {'fileName': 'face', 'type':'png'};
 
 app.use(express.static('public'))
 
 app.get('*', (req, res) => res.send('404 Not found'))
 
 io.on('connection', function (socket) {
-    // socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
-      console.log(data);
+    socket.on('image', function (data) {
+      base64ToImage(data.src, path, optionalObj);
+      cloudinary.uploader.upload(path+optionalObj.fileName+"."+optionalObj.type, function(result) {
+        // fs.unlink(filePath);
+        socket.emit("detect", {url: result.url});
+        console.log(result);
+      });
     });
 });
 
